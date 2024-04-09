5f28709fae8d5cab5cf9d6d89569c33357bee8c7
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-09 11:42:05 +0900

    feat(Home): create Home Component and link with routes to Navbar and App root.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ThemeService } from '../services/theme.service';
 import { NgClass } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,14 +22,30 @@ import { MatIconModule } from '@angular/material/icon';
     MatSlideToggleModule,
     NgClass,
     MatIconModule,
+    HomeComponent,
+    AboutMeComponent,
+    MatSidenav,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor(public themeService: ThemeService) {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  toggleTheme() {
+  constructor(public themeService: ThemeService, private router: Router) {}
+
+  ngAfterViewInit() {
+    this.sidenav.openedStart.subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
+
+  toggleTheme(): void {
     this.themeService.updateTheme();
+  }
+
+  routeToHome(contentSection: HTMLElement): void {
+    this.router.navigate(['home']);
+    contentSection.scrollIntoView({ behavior: 'smooth' });
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ThemeService } from '../../services/theme.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,9 +11,13 @@ import { ThemeService } from '../../services/theme.service';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(public themeService: ThemeService) {}
+  constructor(public themeService: ThemeService, private router: Router) {}
 
   toggleTheme() {
     this.themeService.updateTheme();
+  }
+
+  routeToHome(): void {
+    this.router.navigate(['home']);
   }
 }
