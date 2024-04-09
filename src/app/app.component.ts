8680a7f485d6d2f9f1e530b25efc8a8f1678ed06
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-09 09:20:07 +0900

    add Angular material dark mode toggle functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ThemeService } from '../services/theme.service';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatButtonModule,
+    NavbarComponent,
+    MatSlideToggleModule,
+    NgClass,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'code-chicano';
+  constructor(public themeService: ThemeService) {}
+
+  toggleTheme() {
+    this.themeService.updateTheme();
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ThemeService } from '../../services/theme.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(public themeService: ThemeService) {}
 
+  toggleTheme() {
+    this.themeService.updateTheme();
+  }
 }
