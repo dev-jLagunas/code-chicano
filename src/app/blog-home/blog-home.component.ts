5859a45fc7591453484093bb2125a5796b3dd797
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-11 18:00:12 +0900

    feat(BlogHome): complete Blog Home component html and styling.

--- src/app/blog-home/blog-home.component.ts
+++ src/app/blog-home/blog-home.component.ts
@@ -3,10 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { BlogService } from '../../services/blog.service';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-blog-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, DatePipe, MatMenuModule],
   templateUrl: './blog-home.component.html',
   styleUrl: './blog-home.component.scss',
 })
