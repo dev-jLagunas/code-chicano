2eef311b6e633ce541882613af58ba7d785d65d3
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-11 15:43:32 +0900

    re-add the router-outlet tag.

--- src/app/blog-home/blog-home.component.ts
+++ src/app/blog-home/blog-home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BlogService } from '../../services/blog.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-blog-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './blog-home.component.html',
-  styleUrl: './blog-home.component.scss'
+  styleUrl: './blog-home.component.scss',
 })
 export class BlogHomeComponent {
+  blogPosts!: any[];
 
+  constructor(private blogService: BlogService, private router: Router) {}
+
+  ngOnInit() {
+    this.blogService.blogPosts.subscribe((posts) => {
+      this.blogPosts = posts;
+    });
+  }
+
+  goToBlogDetail(postId: number) {
+    this.router.navigate(['/blog', postId]);
+  }
 }
