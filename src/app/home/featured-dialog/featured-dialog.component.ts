f57341876bba106a2671b31d2cf4bd810861e453
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-15 10:36:45 +0900

    feat(FeaturedDialog): add basic html and styling to Featured Dialog component.

--- src/app/home/featured-dialog/featured-dialog.component.ts
+++ src/app/home/featured-dialog/featured-dialog.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../../../services/blog.service';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { DatePipe } from '@angular/common';
+import { ThemeService } from '../../../services/theme.service';
 @Component({
   selector: 'app-featured-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule, MatCardModule, DatePipe],
   templateUrl: './featured-dialog.component.html',
   styleUrl: './featured-dialog.component.scss',
 })
 export class FeaturedDialogComponent implements OnInit {
-  blogPosts!: any[];
-  constructor(private blogService: BlogService) {}
+  featuredPost!: any;
+  constructor(
+    private blogService: BlogService,
+    public themeService: ThemeService
+  ) {}
 
   ngOnInit() {
-    this.blogService.blogPosts.subscribe((posts) => {
-      this.blogPosts = posts;
-    });
+    this.featuredPost = this.blogService.getRandomBlogPost();
   }
 }
--- src/app/home/latest-dialog/latest-dialog.component.ts
+++ src/app/home/latest-dialog/latest-dialog.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-latest-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule],
   templateUrl: './latest-dialog.component.html',
-  styleUrl: './latest-dialog.component.scss'
+  styleUrl: './latest-dialog.component.scss',
 })
-export class LatestDialogComponent {
-
-}
+export class LatestDialogComponent {}
