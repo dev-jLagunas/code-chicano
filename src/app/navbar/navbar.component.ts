d9b7fde14a3e5c29206c5ac88b26e303b194eb7c
Juan Lagunas <dev.jlagunas@gmail.com>
2024-10-15 13:19:50 +0900

    refactor: change routing methods to router link routing inside the Navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,36 +1,16 @@
 import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ThemeService } from '../../services/theme.service';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatSlideToggleModule, NgClass],
+  imports: [MatSlideToggleModule, NgClass, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(public themeService: ThemeService, private router: Router) {}
-
-  routeToHome(): void {
-    this.router.navigate(['home']);
-  }
-
-  routeToAbout(): void {
-    this.router.navigate(['aboutme']);
-  }
-
-  routeToContact(): void {
-    this.router.navigate(['contact']);
-  }
-
-  routeToBlogHome(): void {
-    this.router.navigate(['bloghome']);
-  }
-
-  routeToLogin(): void {
-    this.router.navigate(['login']);
-  }
+  constructor(public themeService: ThemeService) {}
 }
