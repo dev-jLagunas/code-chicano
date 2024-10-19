a2756d50d5e0b30b1cba01bb560ae0be75a4beb7
Juan Lagunas <dev.jlagunas@gmail.com>
2024-10-19 10:12:03 +0900

    refactor(BlogEntries component): make sure to unsubscribe from observables where applicable

--- src/app/blog-admin/blog-admin.component.ts
+++ src/app/blog-admin/blog-admin.component.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {
   MatBottomSheet,
   MatBottomSheetModule,
+  MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
 import { BlogEntriesComponent } from './blog-entries/blog-entries.component';
 import { BlogPost } from '../../interface/blog-post';
@@ -61,9 +62,11 @@ export class BlogAdminComponent implements OnInit, OnDestroy {
       content: ['', Validators.required],
     });
 
-    this.blogService.blogPosts$.subscribe((posts) => {
-      this.blogPosts = posts;
-    });
+    this.blogService.blogPosts$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((posts) => {
+        this.blogPosts = posts;
+      });
   }
 
   submitBlogForm() {
@@ -77,43 +80,53 @@ export class BlogAdminComponent implements OnInit, OnDestroy {
         description: this.blogForm.value.description,
         content: this.blogForm.value.content,
       };
-      // FORM BLOG ENTRY EDITING LOGIC
-      if (formData.id) {
-        this.blogService
-          .updateBlogPost(formData)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe({
-            next: () => {
-              alert(`Blog post '${formData.title}' updated successfully`);
-            },
-            error: (e) => {
-              console.error('Error updating blog post:', e);
-            },
-          });
-        // FORM BLOG ENTRY ADD NEW
-      } else {
-        this.blogService
-          .addBlogPost(formData)
-          .pipe(takeUntil(this.destroy$))
-          .subscribe({
-            next: (newPost) => {
-              alert(`Blog post '${newPost.title}' added successfully`);
-            },
-            error: (e) => {
-              console.error('Error adding blog post:', e);
-            },
-          });
-      }
 
-      this.blogForm.reset();
-      this.blogForm.get('date')?.setValue(new Date());
-      this.blogForm.get('id')?.setValue(null);
+      formData.id
+        ? this.resubmitEditedPost(formData)
+        : this.addNewBlogPost(formData);
+
+      this.resetForm();
     } else {
       console.log('Form is not valid');
     }
   }
 
+  addNewBlogPost(formData: BlogPost) {
+    this.blogService
+      .addBlogPost(formData)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (newPost) => {
+          alert(`Blog post '${newPost.title}' added successfully`);
+        },
+        error: (e) => {
+          console.error('Error adding blog post:', e);
+        },
+      });
+  }
+
+  resubmitEditedPost(formData: BlogPost) {
+    this.blogService
+      .updateBlogPost(formData)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => {
+          alert(`Blog post '${formData.title}' updated successfully`);
+        },
+        error: (e) => {
+          console.error('Error updating blog post:', e);
+        },
+      });
+  }
+
+  resetForm() {
+    this.blogForm.reset();
+    this.blogForm.get('date')?.setValue(new Date());
+    this.blogForm.get('id')?.setValue(null);
+  }
+
   editPost(post: any) {
+    this.inEditMode = true;
     this.blogForm.setValue({
       title: post.title,
       subtitle: post.subtitle,
@@ -123,19 +136,26 @@ export class BlogAdminComponent implements OnInit, OnDestroy {
       id: post.id,
       content: post.content || '',
     });
-    this.inEditMode = true;
   }
 
   openBottomSheet() {
-    let bottomSheetRef = this._bottomSheet.open(BlogEntriesComponent, {
+    const bottomSheetRef = this._bottomSheet.open(BlogEntriesComponent, {
       data: { blogPosts: this.blogPosts },
     });
 
-    bottomSheetRef.afterDismissed().subscribe((post) => {
-      if (post) {
-        this.editPost(post);
-      }
-    });
+    this.handleBottomSheetDismissal(bottomSheetRef);
+  }
+
+  // USE BOTTOM SHEET AS OUTPUT SO I CAN RECEIVE THE POST DATA TO BE EDITTED
+  handleBottomSheetDismissal(bottomSheetRef: MatBottomSheetRef<any>) {
+    bottomSheetRef
+      .afterDismissed()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((post) => {
+        if (post) {
+          this.editPost(post);
+        }
+      });
   }
 
   ngOnDestroy(): void {
